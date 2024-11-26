09928495ac8861440e59720c9a1067b255a7b0d9
Bryan Godinez Jimenez <jimenezbryan629@gmail.com>
2024-11-26 09:43:29 -0500

    ADDED Category class and removed SA classes.

---
+++ src/Models/Category.java
@@ -0,0 +1,24 @@
+package Models;
+
+public class Category {
+    String categoryId;
+    String name;
+
+    public Category(Stirng categoryId, Stirng name ) {
+        this.categoryId = categoryId;
+        this.name = name;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public setName(String name) {
+        this.name = name;
+    }
+
+}

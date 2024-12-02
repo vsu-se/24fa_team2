7b7acd93517bb62ce2b8765d864fab6973b09398
Wyatt Price <wyattprice02@gmail.com>
2024-12-02 13:05:38 -0500

    REFACTOR User class

--- src/Models/User.java
+++ src/Models/User.java
@@ -1,37 +1,51 @@
 package Models;
+
 public class User {
     private String username;
     private String password;
     private boolean canSell;
     private boolean canBid;
 
+    // Constructor
     public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.canSell = true;  
-        this.canBid = true;
+        this.canSell = false; // Default value
+        this.canBid = false;  // Default value
     }
 
-    //getters and setters
-    public String getUsername(){
+    // Getters and setters
+    public String getUsername() {
         return username;
     }
-    public boolean getCanSell(){
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isCanSell() {
         return canSell;
     }
-    public boolean getCanBid(){ 
+
+    public void setCanSell(boolean canSell) {
+        this.canSell = canSell;
+    }
+
+    public boolean isCanBid() {
         return canBid;
     }
 
-    public void setCanSell(boolean canSell){
-        this.canSell = canSell;
-    }
-    public void setCanBid(boolean canBid){
+    public void setCanBid(boolean canBid) {
         this.canBid = canBid;
     }
 
-    // Password verification
-    public boolean verifyPassword(String password) {
-        return this.password.equals(password);
-    }
+
 }
